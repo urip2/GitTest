d11d0d59fd63082a660a050ab063b5da1cd59f7f
Urip <uripsoft@gmail.com>
2018-01-04 12:05:56 +0500

    Revers

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Hello Urip!")
-        print("hello world!")
         super.viewDidLoad()
     }
 
